311d0fc01326d47809b3e26ad96e02dcb93af776
Kyle Chesney <kyle.2493@gmail.com>
2024-09-10 13:33:16 -0400

    Initial template files

---
+++ ExternalModule.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Vanderbilt\RCC2024Demo;
+
+use ExternalModules\AbstractExternalModule;
+use REDCap;
+
+class ExternalModule extends AbstractExternalModule {
+
+    function redcap_every_page_top($project_id) {
+        $url = $_SERVER['REQUEST_URI'];
+        var_dump($url);
+    }
+
+
+    protected function includeCss(string $path) {
+        echo '<link rel="stylesheet" href="' . $this->getUrl($path) . '">';
+    }
+
+
+    protected function includeJs(string $path) {
+        echo '<script src="' . $this->getUrl($path) . '"></script>';
+    }
+
+
+    protected function setJsSettings(array $settings) {
+        foreach ($settings as $k => $v) {
+            $this->framework->tt_addToJavascriptModuleObject($k, $v);
+        }
+    }
+
+
+    function cron1($cron_info) {
+        echo "I am a cron!";
+        sleep(60*5 + 30);
+        return true;
+    }
+}
---
+++ pages/project_dashboard_page.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Vanderbilt\RCC2024Demo;
+
+echo "Hello world";
