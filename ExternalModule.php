3f82760d72f0b2289155f1e9198a19f4e8d68a18
Kyle Chesney <kyle.2493@gmail.com>
2024-09-10 20:53:02 -0400

    Beginning of dashboard functionality

--- ExternalModule.php
+++ ExternalModule.php
@@ -35,4 +35,34 @@ class ExternalModule extends AbstractExternalModule {
         sleep(60*5 + 30);
         return true;
     }
+
+    function compareProjectData(\Project $srcProject, \Project $dstProject) {
+        $returnRecords = []
+        $srcData = $this->retrieveProjectData($srcProject);
+        $dstData = $this->retrieveProjectData($dstProject);
+
+        foreach ($srcData as $srcRecord => $data) {
+            if (isset($dstData[$srcRecord])) {
+                $returnRecords[$srcRecord] = 
+            }
+        }
+    }
+
+    function retrieveProjectData(\Project $project) {
+        $returnData = [];
+        $projectData = \REDcap::getData([
+            'project_id' => $project->project_id,
+            'return_format' => 'json-array'
+        ]);
+
+        if (empty($projectData['errors'])) {
+            foreach ($projectData as $data) {
+                if ($data[$project->table_pk] != "") {
+                    $returnData[$data[$project->table_pk]] = $data;
+                }
+            }
+        }
+
+        return $returnData;
+    }
 }
